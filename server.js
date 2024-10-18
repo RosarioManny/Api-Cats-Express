94019d74c1bb61e2507a8174c8983fd0e4b2bb09
RosarioManny <rosario.emm47@gmail.com>
2024-10-18 10:34:00 -0400

    Created server and MongoDB

---
+++ server.js
@@ -0,0 +1,24 @@
+import dotenv from 'dotenv';
+dotenv.config();
+import express  from 'express';
+import db from "./db/connection.js" // let db = mongoose.connection
+import chalk from "chalk";
+import logger from "morgan";
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+//TBU: Add mor middleware (cors, express.JSON)
+app.use(logger("dev"))
+
+// ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+// Routes go here
+// ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+
+db.on('connected', () => {
+    console.clear();
+    console.log(chalk.magenta("Connected to MongoDB"));
+});
+app.listen(PORT, () => {
+  console.log(`The express app is ready on port ${PORT}!`);
+});
